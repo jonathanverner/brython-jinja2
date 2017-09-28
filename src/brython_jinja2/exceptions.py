92040f5710d8033a7154fbcb390ea7381ded2d1a
Jonathan L. Verner <jonathan.verner@matfyz.cz>
2017-09-28 17:17:20 +0200

    Refactor exception context handling.

--- src/brython_jinja2/exceptions.py
+++ src/brython_jinja2/exceptions.py
@@ -1,84 +1,23 @@
 from .utils import Location
 
-class LocatedError(BaseException):
+class LocatedError(Exception):
     def __init__(self, message, src=None, location=None):
-        self.src=src
-        if type(location) != int or src is None:
-            self.loc=location
+        if isinstance(location, Location):
+            self.loc = location
+        elif type(location) == int:
+            if src is None:
+                self.loc = Location(pos = location)
+            else:
+                self.loc = Location.location_from_pos(src, location)
         else:
-            self.loc = Location.location_from_pos(self.src, location)
+            self.loc = Location(src)
         self.message = message
         self.context_lines = 4
-        try:
-            self.print()
-        except Exception as ex:
-            print("EX:", str(ex))
-        message = self.__str__()
-        super().__init__(message)
-    
-    def context(self):
-        if self.loc is None:
-            ln="src: "+self.src[:70]
-            if len(self.src) > 70:
-                ln +=" ..."
-            return [ln]
-            
-        ln = self.loc.line
-        col = self.loc.column
-        
-        # Get the Context
-        src_lines = self.src.split('\n')
-        
-        # If there is just a single line, don't bother with line numbers and context lines
-        if len(src_lines) < 2:
-            if isinstance(self.loc, Location):
-                col = self.loc.column
-            else:
-                col = int(self.loc)
-            return ["src: "+self.src,"     "+" "*col+"^"]
-        
-        start_ctx = max(ln-self.context_lines,0)
-        end_ctx = min(ln+self.context_lines+1,len(src_lines))
-        prev_lines = src_lines[start_ctx:ln]
-        post_lines = src_lines[ln+1:end_ctx]
-        
-        # Get the current line with a caret indicating the column
-        cur_lines = ['', src_lines[ln], " "*col+"^"]
-
-        
-        # Prepend line numbers & current line marker
-        line_num_len = len(str(end_ctx))
-        for i in range(len(prev_lines)):
-            prev_lines[i] = '  '+str(start_ctx+i).ljust(line_num_len+2) + prev_lines[i]
-            
-        cur_lines[1] = '> '+str(ln).ljust(line_num_len)+ cur_lines[1]
-        cur_lines[2] = '  '+''.ljust(line_num_len) + cur_lines[2]
-
-        for i in range(len(post_lines)):
-            post_lines[i] = '  '+str(ln+i).ljust(line_num_len+2) + post_lines[i]
-            
-        return prev_lines+post_lines+cur_lines
-            
-        
-            
-    def print(self):
-        print(str(self))
-        
+       
     def __str__(self):
         lines = []
-        if self.loc is None:
-            lines.append(type(self).__name__+": "+self.message)
-        else:
-            lines.append(type(self).__name__+" at "+str(self.loc)+": "+self.message)
-        if self.src is not None:
-            try:
-                #print("CONTEXT:", self.context())
-                lines.extend(self.context())
-                #print("LINES:", lines)
-            except Exception as ex:
-                print(lines)
-                print(self.context())
-                print("EXCEPTION", ex)
+        lines.append(type(self).__name__+" at "+str(self.loc)+": "+self.message)
+        lines.extend(self.loc.context(num_ctx_lines=self.context_lines))
         return "\n".join(lines)
         
     
--- src/brython_jinja2/utils/__init__.py
+++ src/brython_jinja2/utils/__init__.py
@@ -1,12 +1,13 @@
 """
-    The template submodule provides data-binding utilities.
+    Miscellaneous utility classes and functions.
 """
+
 
 class Location:
     
     @classmethod
     def location_from_pos(cls, src, pos, name=None, filename=None):
-        loc = Location(name=name, filename=filename, ln=0, col=0, pos=0)
+        loc = Location(src, name=name, filename=filename, ln=0, col=0, pos=0)
         for c in src:
             loc._inc_pos()
             if c == '\n':
@@ -15,7 +16,8 @@ class Location:
                 break
         return loc
         
-    def __init__(self, name=None, filename=None, ln=0, col=0, pos=0):
+    def __init__(self, src='', name=None, filename=None, ln=0, col=0, pos=0):
+        self._src = src
         self._name = name
         self._fname = filename
         self._ln = ln
@@ -43,8 +45,41 @@ class Location:
         self._col = 0
         
     def clone(self):
-        return Location(name=self._name, filename=self._fname, ln=self._ln, col=self._col, pos=self._pos)
+        return Location(self._src, name=self._name, filename=self._fname, ln=self._ln, col=self._col, pos=self._pos)
         
+    def context(self, num_ctx_lines=4):
+        ln = self.line
+        col = self.column
+        
+        # Get the Context
+        src_lines = self._src.split('\n')
+        
+        # If there is just a single line, don't bother with line numbers and context lines
+        if len(src_lines) < 2:
+            return ["src: "+self._src,"     "+" "*col+"^"]
+        
+        start_ctx = max(ln-num_ctx_lines,0)
+        end_ctx = min(ln+num_ctx_lines+1,len(src_lines))
+        prev_lines = src_lines[start_ctx:ln]
+        post_lines = src_lines[ln+1:end_ctx]
+        
+        # Get the current line with a caret indicating the column
+        cur_lines = ['', src_lines[ln], " "*col+"^"]
+
+        
+        # Prepend line numbers & current line marker
+        line_num_len = len(str(end_ctx))
+        for i in range(len(prev_lines)):
+            prev_lines[i] = '  '+str(start_ctx+i).ljust(line_num_len+2) + prev_lines[i]
+            
+        cur_lines[1] = '> '+str(ln).ljust(line_num_len)+ cur_lines[1]
+        cur_lines[2] = '  '+''.ljust(line_num_len) + cur_lines[2]
+
+        for i in range(len(post_lines)):
+            post_lines[i] = '  '+str(ln+i).ljust(line_num_len+2) + post_lines[i]
+            
+        return prev_lines+post_lines+cur_lines
+    
     def __str__(self):
         ret = '{ln}, {col}'.format(ln=self.line, col=self.column)
         if self._fname is not None:
